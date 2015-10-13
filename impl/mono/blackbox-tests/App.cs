d683d539dc6bc7eb9c5738429ee3244863bb6471
alexro <alex.rodygin@mongrow.com>
2015-10-13 01:06:03 +0100

    Changes line endings

--- impl/mono/blackbox-tests/App.cs
+++ impl/mono/blackbox-tests/App.cs
@@ -28,5 +28,6 @@ namespace sb_bbt
             new TestFetch(this).Run();
             new TestMake(this).Run();
         } 
+
     }
 }
--- impl/mono/blackbox-tests/Utils/Fs.cs
+++ impl/mono/blackbox-tests/Utils/Fs.cs
@@ -67,5 +67,6 @@ namespace sb_bbt.Utils
             }
             return false;
         }
+
     }
 }
