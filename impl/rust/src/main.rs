e51b70dc5d9462eb1374c973e4969c228733bd3a
Kalita Alexey <kalita.alexey@outlook.com>
2015-10-14 09:37:58 +0400

    Fixed #4 #5

--- impl/rust/src/main.rs
+++ impl/rust/src/main.rs
@@ -7,6 +7,9 @@ use sb::commands::make_command::MakeCommand;
 use std::env::current_dir;
 use std::path::{Path, PathBuf};
 
+const DEFAULT_OS: &'static str = "debian";
+const DEFAULT_LAYER: &'static str = "jekyll";
+
 fn main() {
     let (app_path, options, mut arguments) = parse_options();
 
@@ -23,18 +26,16 @@ fn main() {
     let command: &str = &command;
     match command {
         "find" => {
-            assert!(arguments.len() == 2);
-            let os = arguments.remove(0);
-            let layer = arguments.remove(0);
+            let os = get_os_from_arguments_or_default(&mut arguments);
+            let layer = get_layer_from_arguments_or_default(&mut arguments);
             run_command(FindCommand { layer: layer, os: os, slice_root_directory: slice_root_directory })
         },
         "fetch" => {
             run_command(FetchCommand { slice_root_directory: slice_root_directory })
         },
         "make" => {
-            assert!(arguments.len() == 2);
-            let os = arguments.remove(0);
-            let layer = arguments.remove(0);
+            let os = get_os_from_arguments_or_default(&mut arguments);
+            let layer = get_layer_from_arguments_or_default(&mut arguments);
             run_command(MakeCommand { layer: layer, os: os, slice_root_directory: slice_root_directory })
         },
         _ => panic!()
@@ -43,6 +44,22 @@ fn main() {
 
 fn run_command<T>(mut command: T) where T : Command {
     command.run()
+}
+
+fn pop_first_argument_or_take_default(arguments: &mut Vec<String>, default_value: String) -> String {
+    if arguments.len() == 0 {
+        default_value
+    } else {
+        arguments.remove(0)
+    }
+}
+
+fn get_os_from_arguments_or_default(arguments: &mut Vec<String>) -> String {
+    pop_first_argument_or_take_default(arguments, DEFAULT_OS.to_string())
+}
+
+fn get_layer_from_arguments_or_default(arguments: &mut Vec<String>) -> String {
+    pop_first_argument_or_take_default(arguments, DEFAULT_LAYER.to_string())
 }
 
 fn get_slice_root_directory(app_path: & String) -> PathBuf {
