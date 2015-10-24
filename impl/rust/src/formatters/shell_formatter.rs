86347110c8af52d908a9cb5f48ae0c50aa76a472
Kalita Alexey <kalita.alexey@outlook.om>
2015-10-24 23:02:52 +0400

    Added test for shell formatter

--- impl/rust/src/formatters/shell_formatter.rs
+++ impl/rust/src/formatters/shell_formatter.rs
@@ -8,3 +8,31 @@ pub fn code_for_slice(slice: &Slice) -> String {
     }
     string
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+    use helper;
+    use slice::item::Slice;
+    use slice::section::{Kind, Section};
+
+    fn create_slice(run_section_items: Vec<&str>) -> Slice {
+        let items = run_section_items.into_iter().map(|i| i.to_string()).collect::<Vec<_>>();
+        let run_section = Section { kind: Kind::Run, items: items };
+        Slice { name: "Hello".to_string(), path: PathBuf::new(),
+                version: helper::zero_version(), sections: vec![run_section] }
+    }
+
+    #[test]
+    fn code_for_slice() {
+        let slice = create_slice(vec!["apt-get install -q -y wget"]);
+        assert_eq!(super::code_for_slice(&slice), "apt-get install -q -y wget\n");
+
+        let run_section_items = vec!["apt-get install -q -y wget",
+                                     "apt-get install -q -y wget_gui"];
+        let slice = create_slice(run_section_items);
+        assert_eq!(super::code_for_slice(&slice), "apt-get install -q -y wget
+apt-get install -q -y wget_gui
+");
+    }
+}
