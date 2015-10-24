2788015ebec6cb18feaf0b4e10750f40f8c26b07
Kalita Alexey <kalita.alexey@outlook.om>
2015-10-24 22:47:22 +0400

    Test moved into module

--- impl/rust/src/slice/item.rs
+++ impl/rust/src/slice/item.rs
@@ -85,30 +85,38 @@ impl Ord for Slice {
     }
 }
 
-#[test]
-fn slice_get_os_list_works() {
-    let mut lines: Vec<String> = Vec::new();
-    lines.push("OS".to_string());
-    lines.push("debian-8.2".to_string());
-    lines.push("".to_string());
-    lines.push("FROM".to_string());
-    lines.push("debian:jessie".to_string());
-    let mut sections: Vec<Section> = Vec::new();
-    while !lines.is_empty() {
-        let (section, remaining_lines) = Section::load_from_lines(lines);
-        let section = section.unwrap();
-        sections.push(section);
-        lines = remaining_lines;
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+    use helper;
+    use slice::item::Slice;
+    use slice::section::{Kind, Section};
+
+    #[test]
+    fn slice_get_os_list_works() {
+        let mut lines: Vec<String> = Vec::new();
+        lines.push("OS".to_string());
+        lines.push("debian-8.2".to_string());
+        lines.push("".to_string());
+        lines.push("FROM".to_string());
+        lines.push("debian:jessie".to_string());
+        let mut sections: Vec<Section> = Vec::new();
+        while !lines.is_empty() {
+            let (section, remaining_lines) = Section::load_from_lines(lines);
+            let section = section.unwrap();
+            sections.push(section);
+            lines = remaining_lines;
+        }
+        assert_eq!(sections.len(), 2);
+        let slice = Slice {
+            name: "Slice".to_string(),
+            path: PathBuf::new(),
+            version: helper::zero_version(),
+            sections: sections,
+        };
+        let os_list = slice.section(Kind::Os);
+        assert_eq!(os_list.len(), 1);
+        let os = *os_list.first().unwrap();
+        assert_eq!(os, "debian-8.2");
     }
-    assert_eq!(sections.len(), 2);
-    let slice = Slice {
-        name: "Slice".to_string(),
-        path: PathBuf::new(),
-        version: Version::parse("0.0.0").unwrap(),
-        sections: sections,
-    };
-    let os_list = slice.section(Kind::Os);
-    assert_eq!(os_list.len(), 1);
-    let os = *os_list.first().unwrap();
-    assert_eq!(os, "debian-8.2");
 }
